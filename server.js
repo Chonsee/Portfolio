68b16a48fbcb26eac6cb13547a602ee9e9e828b0
Chauncey Loving <66961068+Chonsee@users.noreply.github.com>
2023-06-01 02:36:26 +0000

    Refactored a bunch of code to be more dynamic as i change and update routes. Created basic pages for the rest of my navbar links so that there are no dead links. Moved login logic to the footer of each page.

--- server.js
+++ server.js
@@ -2,43 +2,32 @@ import 'dotenv/config';
 import express from 'express';
 import { ClerkExpressWithAuth, ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 const app = express();
-
-// set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // Define routes
-app.get('/', async (req, res) => {
-  let params = {
-      clerk_pk: process.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
-      title: "Chauncey Vaughn Loving - Full Stack Developer"
-    }
-  res.render('login', params);
-});
+const routes = new Map();
+routes.set('index', { uri: '/', isPublic: true, title: 'Home' });
+routes.set('about', { uri: '/about', isPublic: true, title: 'About' });
+routes.set('experience', { uri: '/experience', isPublic: true, title: 'Experience' });
+routes.set('contact', { uri: '/contact', isPublic: false, title: 'Contact' });
 
-app.get('/login', async (req, res) => {
-  let params = {
-      clerk_pk: process.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
-      title: "Login"
-    }
-  res.render('login', params);
-});
-
-// Use the strict middleware that raises an error when unauthenticated
-app.get(
-  '/contact-me',
-  ClerkExpressRequireAuth({
-    // ...options
-  }),
-  (req, res) => {
+routes.forEach((value, key) => {
+  let clerkMiddleware = value.isPublic ? ClerkExpressWithAuth({}) : ClerkExpressRequireAuth({});
+  
+  app.get(
+    value.uri, 
+    clerkMiddleware,
+    async (req, res) => {
     let params = {
       clerk_pk: process.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
-      title: "Contact Me"
+      title: value.title,
+      routes: routes,
+      isAuthenticated: req.auth?.userId?.length,
     }
-    res.render('contact-me', params);
-  }
-);
-
-app.use(express.static('public'));
+    res.render(key, params);
+  });
+});
 
 // Start the server
 app.listen(3000 || process.env.PORT, () => {});
